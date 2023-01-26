44a1080095f6d3faffb39f060f41e9c2b945aabf
ryudb0 <ryudb030@gmail.com>
2023-01-26 18:41:48 +0900

    ch05 예제

---
+++ src/Ex5_14.java
@@ -0,0 +1,19 @@
+
+public class Ex5_14 {
+	public static void main(String[] args) {
+		String src = "ABCDE";
+		
+		for(int i=0; i<src.length(); i++) {
+			char ch = src.charAt(i);
+			System.out.println("src.charAt("+i+"):"+ch);
+		}
+		
+		char[] chArr = src.toCharArray();
+		System.out.println(chArr);
+		
+		
+		
+		
+	}
+
+}
